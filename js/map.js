bce8f1a24cc1980f824148487bef68861659028e
AliceBalfanz <alicebalfanz@gmail.com>
2017-07-03 13:44:50 +0200

    featurelayer added

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,16 +6,15 @@
 
 
 // ============
-    // Esri-Leaflet 
+    // Esri-Leaflet
     // ============
 
     var map = L.map('map',{zoomControl : false,zoomAnimation : false, minZoom : 7,maxBounds : [[50.31,5.77],[52.62,9.46]]}).setView([51.422080, 8.022025],8),
       layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map),
       // layerLabels = L.esri.basemapLayer('xxxLabels').addTo(map);
       layerLabels = null,
-      worldTransportation = L.esri.basemapLayer('ImageryTransportation');      
-	  
-	  
+      worldTransportation = L.esri.basemapLayer('ImageryTransportation');
+
 
     function setBasemap(basemap) {
       if (layer) {
@@ -36,12 +35,12 @@
         layerLabels = L.esri.basemapLayer(basemap + 'Labels');
         map.addLayer(layerLabels);
       }
-        
+
       // add world transportation service to Imagery basemap
       if (basemap === 'Imagery') {
-        worldTransportation.addTo(map);            
+        worldTransportation.addTo(map);
       } else if (map.hasLayer(worldTransportation)) {
-        // remove world transportation if Imagery basemap is not selected    
+        // remove world transportation if Imagery basemap is not selected
         map.removeLayer(worldTransportation);
       }
     }
@@ -52,8 +51,8 @@
 
     //var searchControl = L.esri.Geocoding.Controls.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
     var searchControl = L.esri.Geocoding.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: true}).addTo(map);
-    
-    searchControl.on('results', function(data){ 
+
+    searchControl.on('results', function(data){
       if (data.results.length > 0) {
         var popup = L.popup()
           .setLatLng(data.results[0].latlng)
@@ -62,17 +61,37 @@
         map.setView(data.results[0].latlng)
       }
     })
-	
+
 	L.easyPrint({
 			title: 'Click to print the map',
 			position: 'topright'
 		}).addTo(map);
-        
-        
-    //adding StreamLayer    
-	var buses = L.esri.streamFeatureLayer({
-		url: 'https://geoeventsample3.esri.com:6443/arcgis/rest/services/LABus/StreamServer'
-	}).addTo(map);
-console.log("added streamlayer");
 
- 
+//adding Feature layer for Workers
+
+var workers = L.esri.featureLayer({
+  url:'https://services1.arcgis.com/W47q82gM5Y2xNen1/arcgis/rest/services/WorkerFeature/FeatureServer/0'
+}).addTo(map);
+
+//adding Feature layer for Inaccessible Roads
+var inaccessibleRoads = L.esri.featureLayer({
+  url:'https://services1.arcgis.com/W47q82gM5Y2xNen1/arcgis/rest/services/inaccessibleRoads01/FeatureServer/0'
+}).addTo(map);
+
+/*map.on(L.Draw.Event,CREATED; function (e)){
+  var feature = {
+    type : 'Feature',
+    geometry : {
+      type : 'Point',
+      coordinates : [7.574620, 51.982685],
+    },
+    properties : {
+      created by : 'alicja',
+      description : 'asjdojas'
+    }
+  };
+
+  inaccessibleRoads.addFeature(feature);
+}*/
+
+console.log("added FeatureLayers");
