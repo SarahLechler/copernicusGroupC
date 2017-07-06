542de915dedac791b7b5e35212f1f206fa4ba3d3
MojioMS <MaurinRadtke@googlemail.com>
2017-07-06 16:09:18 +0200

    precipitation layer

--- js/map.js
+++ js/map.js
@@ -12,18 +12,46 @@
 var map = L.map('map', {zoomControl: false, zoomAnimation: false,
     //minZoom: 7, maxBounds:[[50.31, 5.77], [52.62, 9.46]]
 }).setView([51.422080, 8.022025], 8),
-        layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+        layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map),
         // layerLabels = L.esri.basemapLayer('xxxLabels').addTo(map);
         layerLabels = null,
         worldTransportation = L.esri.basemapLayer('ImageryTransportation'),
-        precip_layer = L.tileLayer("http://{s}.tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=f76f082faa2e7e033b8eced98e9132ae"),
-        layers = L.control.layers(layer, precip_layer).addTo(map);
+        precip_layer = L.tileLayer("http://{s}.tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=f76f082faa2e7e033b8eced98e9132ae");
 
+
+console.log("#1");
+if (map.hasLayer(precip_layer)) {
+    console.log("#2");
+    map.remove(precip_layer);
+    console.log("#3");
+    map.addLayer(precip_layer);
+} else {
+    console.log("#4");
+    map.addLayer(precip_layer);
+    console.log("#5");
+}
+
+var show_precip = true;
+
+function getPrecipitation() {
+    show_precip = !show_precip;
+    console.log("precipitation activate!");
+    if (layer)
+        map.removeLayer(layer);
+    if (precip_layer)
+        map.removeLayer(precip_layer);
+    map.addLayer(layer);
+    if (show_precip)
+        map.addLayer(precip_layer);
+}
+;
 
 function setBasemap(basemap) {
     if (layer) {
         map.removeLayer(layer);
     }
+    if (precip_layer)
+        map.removeLayer(precip_layer);
     if (basemap === 'OpenStreetMap') {
         layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
     } else {
@@ -37,7 +65,6 @@ function setBasemap(basemap) {
     if (basemap === 'ShadedRelief' || basemap === 'Oceans' || basemap === 'Gray' || basemap === 'DarkGray' || basemap === 'Imagery' || basemap === 'Terrain') {
         layerLabels = L.esri.basemapLayer(basemap + 'Labels');
         map.addLayer(layerLabels);
-        map.addLayer(precip_layer);
     }
 
     // add world transportation service to Imagery basemap
@@ -46,6 +73,9 @@ function setBasemap(basemap) {
     } else if (map.hasLayer(worldTransportation)) {
         // remove world transportation if Imagery basemap is not selected    
         map.removeLayer(worldTransportation);
+    }
+    if (show_precip) {
+        map.addLayer(precip_layer);
     }
 }
 
@@ -60,7 +90,7 @@ searchControl.on('results', function (data) {
     //var searchControl = L.esri.Geocoding.Controls.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
     var searchControl = L.esri.Geocoding.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: true}).addTo(map);
 
-})
+});
 
 L.easyPrint({
     title: 'Click to print the map',
@@ -69,28 +99,23 @@ L.easyPrint({
 
 //adding Feature layer for Workers
 
+//adding Feature layer for Workers
+
+
+
 var workers = L.esri.featureLayer({
     url: 'https://services1.arcgis.com/W47q82gM5Y2xNen1/arcgis/rest/services/WorkerFeature/FeatureServer/0'
 }).addTo(map);
+
+workers.bindPopup(function (layer) {
+    return L.Util.template('<p>Name: {Name}<br>Description: {Descriptoon_Task}</p>', layer.feature.properties);
+});
 
 //adding Feature layer for Inaccessible Roads
 var inaccessibleRoads = L.esri.featureLayer({
     url: 'https://services1.arcgis.com/W47q82gM5Y2xNen1/arcgis/rest/services/inaccessibleRoads01/FeatureServer/0'
 }).addTo(map);
 
-/*map.on(L.Draw.Event,CREATED; function (e)){
- var feature = {
- type : 'Feature',
- geometry : {
- type : 'Point',
- coordinates : [7.574620, 51.982685],
- },
- properties : {
- created by : 'alicja',
- description : 'asjdojas'
- }
- };
-  
- inaccessibleRoads.addFeature(feature);
- }*/
-
+inaccessibleRoads.bindPopup(function (layer) {
+    return L.Util.template('<p>Name: {Name}<br>Description: {Descriptoon_Task}</p>', layer.feature.properties);
+});
