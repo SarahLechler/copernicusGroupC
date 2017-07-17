fd881e2861a2806327899d09c2d034af2c13580a
SarahLechler <s_lech05@uni-muenster.de>
2017-07-17 16:35:15 +0200

    Revert "timeenabling gauging stations"

--- js/time.js
+++ js/time.js
@@ -1,32 +1,18 @@
 function selectedTime() {
-    console.log("selectedTime");
     var val = document.getElementById('range').value;
     console.log(unique_date[val]);
     console.log(val);
+
     return unique_date[val];
 }
 
-function selectingTime() {
-    var val = document.getElementById('range').value;
-    // val
-    for (var key in allPegelData) {
-        // skip loop if the property is from prototype
-        if (!allPegelData.hasOwnProperty(key))
-            continue;
-        var obj = allPegelData[key];
-        if (!obj.marker)
-            continue;
-        obj.marker.setStyle(
-                {
-                    color: '#0000FF',
-                    weight: 2,
-                    fill: true,
-                    fillColor: '#' + getColor(obj.min, obj.max, obj[val]) + "FF",
-                    fillOpacity: 1,
-                    radius: 10,
-                    opacity: 1
-                }
-        );
-    }
-    return unique_date[val];
+function selectedTimeEnd() {
+    console.log("onended");
+}
+
+function test124(){
+    console.log("ondrag");
+}
+function test125(){
+    console.log("onSlide");
 }
