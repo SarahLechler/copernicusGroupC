0309f2a713fb9596a45c289773c0daec0d48c52f
Milan <mkgreven@aol.com>
2017-07-24 13:26:32 +0200

    time responsive x-axis of gauging stations graphs

--- js/waterlevel.js
+++ js/waterlevel.js
@@ -332,7 +332,8 @@ function getChart(station_name) {
             var date_slider = selectedTime();
             for (var i in result) {
                 if (date_slider == (result[i].timestamp).substr(0, 10)) {
-                    datapoints.push({label: result[i].timestamp.substr(0, 10), y: result[i].value, toolTipContent: result[i].timestamp + " : " + result[i].value});
+					var timeLabel = result[i].timestamp.substring(11,16);
+                    datapoints.push({label: timeLabel, y: result[i].value, toolTipContent: result[i].timestamp + " : " + result[i].value});
                 }
 
             }
@@ -402,7 +403,7 @@ $.ajax({
             if (result[current].longitude)
                 currStationData.longitude = result[current].longitude;
             allPegelData['' + result[current].shortname] = currStationData;
-            //getStationData(result[current].shortname, currStationData.latitude, currStationData.longitude);
+            getStationData(result[current].shortname, currStationData.latitude, currStationData.longitude);
 //                    for (i in result) {
 //                        if (date_slider == (result[i].timestamp).substr(0, 10)) {
 //                            datapoints.push({label: result[i].timestamp.substr(0, 10), y: result[i].value, toolTipContent: result[i].timestamp + " : " + result[i].value});
