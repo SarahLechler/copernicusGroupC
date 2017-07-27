80704d6ea4628ba459f74788bc686d3e83f54f56
SarahLechler <lechler.sarah@gmail.com>
2017-07-27 14:35:05 +0200

    changes weatherdata

--- js/weatherdata.js
+++ js/weatherdata.js
@@ -12,7 +12,7 @@ function getWeather() {
     var weather_button = document.getElementById('weather_button');
     if (!weather_pressed) {
         
-        var URL = "http://52.59.157.69:8000//weather2017_07_22.json";
+        var URL = "http://52.59.157.69:8000//weather2017_07_26.json";
         //var URL = "http://api.openweathermap.org/data/2.5/box/city?bbox=5.77,50.31,9.46,52.62,9&APPID=3e4e9e28f3ee43f6058e966b7f2be8c6";
         $.ajax({
             url: URL,
@@ -22,7 +22,7 @@ function getWeather() {
             crossDomain: true,
             success: function (result) {
                 console.log(result);
-                addToMap(result);
+                //addToMap(result);
             },
             error: function (xhr, textStatus, errorThrown) {
                 alert("Unable to fetch Server data")
